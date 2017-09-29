8b93fbc40a28b2e07343ae567589b5e058374b8e
David Ackerman <david.s.ackerman@gmail.com>
2017-09-29 17:18:20 -0600

    Scripts to parse meta data from the html files, text from all files, including pdf, doc, and ppt, split that text up a bit, and put it all in a local sqlite db.

---
+++ scripts/wrangling/make-db.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require "sqlite3"
+
+script_path = File.expand_path(File.dirname(__FILE__))
+data_path = File.join(script_path,"..","..","data")
+db_path = File.join(data_path, "processed", "docs.db")
+
+db = SQLite3::Database.new(db_path)
+
+db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS docs(
+    id INTEGER PRIMARY KEY,
+    docname VARCHAR(256),
+    content TEXT,
+    error VARCHAR(256)
+  );
+SQL
+
+db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS segments(
+    id INTEGER PRIMARY KEY,
+    docid INTEGER,
+    seq INTEGER,
+    content TEXT,
+    UNIQUE(docid, seq),
+    FOREIGN KEY(docid) REFERENCES docs(id)
+  );
+SQL
+
+db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS segmentmeta(
+    id INTEGER PRIMARY KEY,
+    key VARCHAR(128),
+    segmentid INTEGER,
+    value TEXT,
+    FOREIGN KEY(segmentid) REFERENCES segments(id),
+    UNIQUE(segmentid,key)
+  );
+SQL
+
+db.execute <<-SQL
+CREATE TABLE IF NOT EXISTS docmeta(
+  id INTEGER PRIMARY KEY,
+  key VARCHAR(128),
+  docid INTEGER,
+  value TEXT,
+  FOREIGN KEY(docid) REFERENCES docs(id),
+  UNIQUE(docid,key)
+);
+SQL
