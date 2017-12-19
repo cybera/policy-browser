96824031d3f0222b63c55ce0663c84198181d97d
David Ackerman <david.s.ackerman@gmail.com>
2017-12-18 21:48:15 -0700

    more refactoring for readability

---
+++ scripts/wrangling/html_submission.py
@@ -0,0 +1,29 @@
+from scrapy.selector import Selector
+
+class HTMLSubmission:
+  def __init__(self, text):
+    self.doc = Selector(text=text)
+
+  def top_level(self, fieldname):
+    element = self.doc.xpath("//div[contains(text(),'%s')]/*" % fieldname)
+    text = element.xpath("text()").extract_first()
+    if text:
+     return text.strip()
+    else:
+      return None
+
+  def client_info(self, fieldname):
+    element = self.doc.xpath("//div[contains(text(),'Client information')]/following::div[contains(text(),'%s')][1]/*" % fieldname)
+    text = element.xpath("text()").extract_first()
+    if text:
+      return text.strip()
+    else:
+      return None
+
+  def designated_representative(self, fieldname):
+    element = self.doc.xpath("//div[contains(text(),'Designated representative')]/following::div[contains(text(),'%s')][1]/*" % fieldname)
+    text = element.xpath("text()").extract_first()
+    if text:
+      return text.strip()
+    else:
+      return None
--- scripts/wrangling/neo4j-import.py
+++ scripts/wrangling/neo4j-import.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from neo4j.v1 import GraphDatabase
+from html_submission import HTMLSubmission
 
 uri = "bolt://neo4j:7687"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
@@ -13,42 +14,17 @@ def transaction():
     with session.begin_transaction() as tx:
       yield tx
 
-def xml_top_level(doc, fieldname):
-  element = doc.xpath("//div[contains(text(),'%s')]/*" % fieldname)
-  text = element.xpath("text()").extract_first()
-  if text:
-    return text.strip()
-  else:
-    return None
-
-def xml_client_info(doc, fieldname):
-    element = doc.xpath("//div[contains(text(),'Client information')]/following::div[contains(text(),'%s')][1]/*" % fieldname)
-    text = element.xpath("text()").extract_first()
-    if text:
-      return text.strip()
-    else:
-      return None
-
-def xml_designated_representative(doc, fieldname):
-    element = doc.xpath("//div[contains(text(),'Designated representative')]/following::div[contains(text(),'%s')][1]/*" % fieldname)
-    text = element.xpath("text()").extract_first()
-    if text:
-      return text.strip()
-    else:
-      return None
-
 import os
 import re
-from scrapy.selector import Selector
 
 scrapedir = os.path.join("data", "raw")
 txtdir = os.path.join("data", "processed", "raw_text")
 
-with driver.session() as session:
-  with session.begin_transaction() as tx:
+def merge_core():
+  print("Using filenames to merge PublicProcess, Intervention, and Document nodes")
+  with transaction() as tx:
     for fname in os.listdir(scrapedir):
-      if fname != ".gitignore":
-        print("Importing: %s" % fname)
+      if fname != ".gitignore" and fname != ".DS_Store":
         dtype=fname.rsplit('.', 1)[1]
         the_rest=fname.rsplit('.', 1)[0]
         [public_process_number,case,dmid,name] = the_rest.split('.', 3)
@@ -61,55 +37,64 @@ with driver.session() as session:
           MERGE (d)-[:PART_OF]->(i)
           MERGE (p)-[:RECEIVES]->(i)
         """, ppn=public_process_number, case=int(case), dmid=int(dmid), dtype=dtype, fname=fname)
-    for record in tx.run("""
+
+def merge_raw_text():
+  print("Parsing text and merging as raw_text on Documents")
+  with transaction() as tx:
+    for r in tx.run("""
       MATCH (d:Document) WHERE NOT EXISTS(d.raw_text) RETURN d.name AS name
       """):
-      filepath = os.path.join(scrapedir, record['name'])
-      txtpath = os.path.join(txtdir, "%s.txt" % record['name'])
+      filepath = os.path.join(scrapedir, r['name'])
+      txtpath = os.path.join(txtdir, "%s.txt" % r['name'])
       if os.path.exists(txtpath):
-        print("Importing raw text: " + txtpath)
-        file = open(txtpath, "r", encoding="latin-1")
-        raw_text = file.read()
-        tx.run("MATCH (d:Document { name: $fname }) SET d.raw_text = $raw_text", fname=record['name'], raw_text=raw_text)
+        raw_text = open(txtpath, "r", encoding="latin-1").read()
+        tx.run("""
+          MATCH (doc:Document { name: $fname })
+          SET doc.raw_text = $raw_text
+        """, fname=r['name'], raw_text=raw_text)  
 
 def merge_people():
+  print("Finding People from HTML document submissions related to an Intervention")
   with transaction() as tx:
     results = tx.run("""
       MATCH (i:Intervention)-[:CONTAINS]->(d:Document {type:'html'})
       WHERE NOT (:Person)-[:SUBMITTED]->(i:Intervention) 
       RETURN i.case AS case, d.raw_text AS raw_text
-      """)
+    """)
     for r in results:
-      doc = Selector(text=r["raw_text"])
-      person_name = xml_client_info(doc, "Name")
+      doc = HTMLSubmission(r["raw_text"])
+      person_name = doc.client_info("Name")
       if person_name:
         tx.run("""
-          MATCH (i:Intervention { case: {case}})
-          MERGE (p:Person { name: {name} })
+          MATCH (i:Intervention { case: $case})
+          MERGE (p:Person { name: $name })
           MERGE (p)-[:SUBMITTED]->(i)
-          """, name=person_name, case=r["case"] )
+        """, name=person_name, case=r["case"] )
 
 def merge_organizations():
+  print("Finding Organizations from HTML document submissions related to an Intervention")
   with transaction() as tx:
     results = tx.run("""
       MATCH (person:Person)-[:SUBMITTED]->(i:Intervention)-[:CONTAINS]->(doc:Document {type:'html'})
       WHERE NOT (person)-[:WORKING_FOR]->(:Organization)
       RETURN i.case AS case, doc.raw_text, person.name
-      """)
+    """)
     for r in results:
-      doc = Selector(text=r["doc.raw_text"])
-      person_name = xml_client_info(doc, "Name")
-      org_name = xml_client_info(doc, "On behalf of company")
-      person_title = xml_client_info(doc, "Title")
+      doc = HTMLSubmission(r["doc.raw_text"])
+      person_name = doc.client_info("Name")
+      org_name = doc.client_info("On behalf of company")
+      person_title = doc.client_info("Title")
       if person_name == r["person.name"] and org_name:
         tx.run("""
-          MATCH (p:Person { name: {name} })
-          MERGE (o:Organization { name: {organization_name} })
+          MATCH (p:Person { name: $name })
+          MERGE (o:Organization { name: $org_name })
           MERGE (p)-[r1:WORKING_FOR]->(o)
-          ON CREATE SET r1.title = {title}
+          ON CREATE SET r1.title = $title
           MERGE (o)-[r2:EMPLOYS]->(p)
-          ON CREATE SET r2.title = {title}
-        """, organization_name=org_name, name=person_name, title=person_title)
+          ON CREATE SET r2.title = $title
+        """, org_name=org_name, name=person_name, title=person_title)
 
+merge_core()
+merge_raw_text()
 merge_people()
 merge_organizations()
