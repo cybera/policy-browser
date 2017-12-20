aea9ad2e593c33f8679a6742e1a9db29b9067fcc
David Ackerman <david.s.ackerman@gmail.com>
2017-12-20 14:30:25 -0700

    fixed text extraction under python

--- scripts/wrangling/html_submission.py
+++ scripts/wrangling/html_submission.py
@@ -6,24 +6,32 @@ class HTMLSubmission:
 
   def top_level(self, fieldname):
     element = self.doc.xpath("//div[contains(text(),'%s')]/*" % fieldname)
-    text = element.xpath("text()").extract_first()
+    text = element.xpath(".//text()").extract_first()
     if text:
-     return text.strip()
+     return "".join(text).strip()
     else:
       return None
 
   def client_info(self, fieldname):
     element = self.doc.xpath("//div[contains(text(),'Client information')]/following::div[contains(text(),'%s')][1]/*" % fieldname)
-    text = element.xpath("text()").extract_first()
+    text = element.xpath(".//text()").extract()
     if text:
-      return text.strip()
+      return "".join(text).strip()
     else:
       return None
 
   def designated_representative(self, fieldname):
     element = self.doc.xpath("//div[contains(text(),'Designated representative')]/following::div[contains(text(),'%s')][1]/*" % fieldname)
-    text = element.xpath("text()").extract_first()
+    text = element.xpath(".//text()").extract()
     if text:
-      return text.strip()
+      return "".join(text).strip()
+    else:
+      return None
+
+  def comment(self):
+    element = self.doc.xpath("//div[contains(text(),'Comment')]/following::div[1]")
+    text = element.xpath(".//text()").extract()
+    if text:
+      return "".join(text).strip()
     else:
       return None
