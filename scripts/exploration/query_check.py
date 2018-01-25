1b5dc881ef963006d7d27ad4bef071933fc6a597
Alex Tennant <alextennant@Alexs-MacBook-Pro.local>
2018-01-25 15:24:31 -0700

    Added better descriptions to files

--- scripts/exploration/query_check.py
+++ scripts/exploration/query_check.py
@@ -29,24 +29,20 @@ sentence = "define the internet as a basic service".lower()
 tokens = tokenizer.tokenize(sentence)
 stopped_tokens =  [sentence for sentence in tokens if sentence not in EnglishStopWords]
 
-
+# This builds a vector to your custom sentence
 infer_vector = model.infer_vector(stopped_tokens, steps=20)#, alpha=0.025)
-#infer_vector2 = model2.infer_vector(MyDesire)#, alpha=0.025)
 
-
-#similars2 = model2.docvecs.most_similar(positive=[infer_vector2], topn=30)
+# This finds the most similar vectors. To return more set topn to something larger
 similars = model.docvecs.most_similar(positive=[infer_vector], topn=30)
 
-print("2000 Neurons")
+print("DBOW")
+# This prints the actual sentences from the dataframe based on the tag of the most
+# relevant vectors. locations[0] is a (string) of the integer tag that was given to
+# it in per_sentence_analysis.py 
 for locations in similars:
     middle = int(locations[0])
     print( df["Sentence"][middle-2:middle+2], '\n', "Document: ",
           df["DocumentNumber"][int(locations[0])], "Inner Product: ", locations[1], '\n')
 
-#print("__________________________________________________________________________________-")
-#print()
-#print("1500 Neurons")
-#for locations in similars2:
-#   print( df["Sentence"][int(locations[0])], '\n', "Document: ",
- #         df["DocumentNumber"][int(locations[0])], "Inner Product: ", locations[1], '\n')
+
 
