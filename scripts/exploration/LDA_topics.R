25ff936fb4d4f4a8d3ac6248253e92b9ef4616f7
Tatiana Meleshko <tatianameleshko@Tatianas-MacBook-Pro.local>
2018-02-16 15:19:52 -0700

    changed LDA topics script and markdown

--- scripts/exploration/LDA_topics.R
+++ scripts/exploration/LDA_topics.R
@@ -1,12 +1,12 @@
 library(dplyr)
 library(ggplot2)
-library(tm)
 library(topicmodels)
 library(tidytext)
-library(LDAvis)
 library(stringi)
 library(magrittr)
 library(tsne)
+library(stringr)
+
 #setwd("~/DS/hey-cira/data/processed")
 
 docs <- read.csv("data_english.csv",fileEncoding="UTF-8", stringsAsFactors =F)
@@ -15,154 +15,94 @@ docs_fr <- docs_fr[,c(1,2,4)]
 colnames(docs_fr) <- colnames(docs)
 docs1 <- rbind(docs, docs_fr) 
 
-content <- docs1$content
-docs_corpus <- Corpus(VectorSource(content))
-docs_corpus <- tm_map(docs_corpus, removePunctuation) 
-docs_corpus <- tm_map(docs_corpus, removeNumbers)
-docs_corpus <- tm_map(docs_corpus, tolower)
-docs_corpus <- tm_map(docs_corpus, stripWhitespace)
-docs_corpus <- tm_map(docs_corpus, removeWords, stopwords("english"))
-docs_corpus <- tm_map(docs_corpus, removeWords, stopwords("french"))
-docs_corpus <- tm_map(docs_corpus, content_transformer(gsub), pattern = "next genetration", replacement = "nextgeneration")
-docs_corpus <- tm_map(docs_corpus, content_transformer(gsub), pattern = "non profit", replacement = "nonprofit")
-docs_corpus <- tm_map(docs_corpus, content_transformer(gsub), pattern = "services", replacement = "service")
-docs_corpus <- tm_map(docs_corpus, content_transformer(gsub), pattern = "communities", replacement = "community")
-docs_corpus <- tm_map(docs_corpus, content_transformer(gsub), pattern = "providers", replacement = "provider")
-docs_corpus <- tm_map(docs_corpus, content_transformer(gsub), pattern = "highw", replacement = "highway")
-docs_corpus <- tm_map(docs_corpus, content_transformer(gsub), pattern = "hig", replacement = "high")
-docs_corpus <- tm_map(docs_corpus, content_transformer(gsub), pattern = "highh", replacement = "high")
-docs_corpus <- tm_map(docs_corpus, content_transformer(gsub), pattern = "highwayay", replacement = "highway")
-my_stopwords <-c("can","name","via","first","last","per", "will", "plus","form","next","non","tre",
-                 "forumforresearchandpolicyincommunicationsfrpc","nonno",
-                 "httpwwwstatcangccacansimanosecfalseinitialviewperiodnumberwhenconvertingfrequencyusecalendaryearactionaaapplypeyearoutputfrequencyunchangedexporteridtabbedtablehtmltimeascolumnmbrbgeodmbrbcommdsmonthlangengsyearidemonthstbyvalaccessiblefalseretrlangengmanipulationoptionpercentyear",
-                 "moreonthisigrmarketstudyhere",
-                 "httpsdrivegooglecomfiledbwejdkzpaqrzrhynaqlgunmviewuspsharing" ,
-                 "exchangenodeindependantcarrier", 
-                 "httpsdrivegooglecomfiledbwejdkzpaqyzcmbtzltqtqviewuspsharing",
-                 "crtcparaaug",
-                 "httpsdrivegooglecomfiledbwejdkzpaqxumlzyvwrxyuuviewuspsharing", 
-                 "httpsdrivegooglecomfiledbwejdkzpaqrcrwtusupyvcviewuspsharing",
-                 "httpsnordicitysharepointcomteamsitecnocwirelessreportcnocwirelesssubstitutionfinaldocxtoc",
-                 "openmediaresponsedecember",
-                 "openmediaengagementnetworkpoboxcommercialdrvancouverbccanada",
-                 "openmediaisacommunitybasedorganizationthatsafeguardsthepossibilitiesoftheopeninternet",
-                 "increaseminimumservicetandardstombpsdownloadandmbpsforuploadspeeds",
-                 "httpwwwteluscomenabgethelpservicetermsffhinternetaccessservicetermssupportdoevarhttpwwwteluscomengethelpservicetermsffhinternetaccessservicetermssupportdo",
-                 "httpeceuropaeunewsroomdaedocumentcfmactiondisplaydocid",
-                 "publicabridgedssiresponsestoaugustinterrogatoriestnc",
-                 "httpwwwcrtcgccaengtranscriptstthtm")
-docs_corpus <- tm_map(docs_corpus, removeWords,my_stopwords)
-dtm <- DocumentTermMatrix(docs_corpus) 
+my_stopwords <- data_frame(word = c(as.character(1:10),"nextgeneration", "nono","highw","hig","highh","highwayay", "http", "st", "https"))
 
-term_tfidf <- tapply(dtm$v/slam::row_sums(dtm)[dtm$i], dtm$j, mean) *
-  log2(tm::nDocs(dtm)/slam::col_sums(dtm > 0))
-summary(term_tfidf)
+data_test <- docs1[,c("sha256","content")]
+data_test <- data_test %>% 
+  unnest_tokens(word, content) %>% 
+  anti_join(stop_words)  %>%
+  anti_join(my_stopwords)
 
-reduced.dtm <- dtm[,term_tfidf >= 0.0022]
-summary(slam::col_sums(reduced.dtm))
+data_test <- data_test %>%
+  filter(!str_detect(word, "\\b[[:alpha:]]{15,}\\b"),!str_detect(word, "[[:digit:]]+"))
 
-rowTotals <- apply(reduced.dtm, 1, sum) #Find the sum of words in each Document
-reduced.dtm   <- reduced.dtm[rowTotals> 0, ]
+#Select only words that appear more than 50 times
+words50 <- data_test %>%
+  group_by(word) %>%
+  mutate(word_total = n()) %>%
+  ungroup() %>%
+  filter(word_total > 50)
 
-control_list_gibbs <- list(
-  burnin = 2500,
-  iter = 5000,
-  seed = 0:4,
-  nstart = 5,
-  best = TRUE
-)
+dtm50 <- words50 %>%
+  count(sha256, word) %>%
+  cast_dtm(sha256, word, n)
 
-system.time(model10 <- topicmodels::LDA(reduced.dtm, 10, method = "Gibbs", control =control_list_gibbs))
+modelsize=10
+#modelsize=30
+#modelsize=100
 
-dtm_topics <- tidy(model10, matrix = "beta")
+model <- LDA(dtm50, k = modelsize, control = list(seed = 1234))
 
-dtm_top_terms <- dtm_topics %>%
+doc_topics <- tidy(model, matrix = "beta")
+
+top_terms <- doc_topics %>%
   group_by(topic) %>%
   top_n(10, beta) %>%
   ungroup() %>%
   arrange(topic, -beta)
 
-dtm_top_terms %>%
+top_terms %>%
   mutate(term = reorder(term, beta)) %>%
   ggplot(aes(term, beta, fill = factor(topic))) +
   geom_col(show.legend = FALSE) +
   facet_wrap(~ topic, scales = "free") +
   coord_flip()
 
-topicTerms <- cbind(dtm_top_terms, Rank = rep(1:10))
+#Visialization - maximum gamma
+gamma_dtm <- tidy(model, matrix = "gamma")
+max_per_doc = aggregate(gamma_dtm$gamma,by=list(gamma_dtm$document),max)
+
+ggplot(data=max_per_doc,aes(x, fill=factor(modelsize))) +
+  geom_histogram(bins = 20) +
+  scale_fill_discrete(name = "Number of Topics") + 
+  xlab("maximum gamma per document") +
+  geom_vline(aes(xintercept = 1/modelsize),color="darkred") +
+  labs(title = paste(modelsize," topics"))
+
+####Topiclabels
+dtm_terms <- as.data.frame(topicmodels::terms(model, 10), stringsAsFactors = FALSE)
+topicTerms <- tidyr::gather(dtm_terms, Topic)
+topicTerms <- cbind(topicTerms, Rank = rep(1:10))
 topTerms <- dplyr::filter(topicTerms, Rank < 4)
+topTerms <- dplyr::mutate(topTerms, Topic = stringr::word(Topic, 2))
+topTerms$Topic <- as.numeric(topTerms$Topic)
 topicLabel <- data.frame()
-for (i in 1:10){
-  z <- dplyr::filter(topTerms, topic == i)
+for (i in 1:modelsize){
+  z <- dplyr::filter(topTerms, Topic == i)
   l <- as.data.frame(paste(z[1,2], z[2,2], z[3,2], sep = " " ), stringsAsFactors = FALSE)
   topicLabel <- rbind(topicLabel, l)
   
 }
 colnames(topicLabel) <- c("Label")
 
-gamma_dtm10 <- tidy(model10, matrix = "gamma")
-max_per_doc10 = aggregate(gamma_dtm10$gamma,by=list(gamma_dtm10$document),max)
-
-ggplot(data=max_per_doc10,aes(x, fill=factor(10))) +
-  geom_histogram(bins = 20) +
-  scale_fill_discrete(name = "Number of Topics") + 
-  xlab("maximum gamma per document") +
-  geom_vline(aes(xintercept = 1/10),color="darkred") +
-  labs(title = "Gamma for 10 topics")
-
-gamma_dtm10 <- gamma_dtm10 %>% 
-  group_by(document) %>%
-  filter(gamma==max(gamma))
-table(gamma_dtm10$topic)
-
-
-zero_docs <- unname(which(rowTotals==0))
-
-svd_tsne <- function(x) tsne(svd(x)$u)
-topicmodels_json_ldavis <- function(fitted, corpus, doc_term){
-  phi <- posterior(fitted)$terms %>% as.matrix
-  theta <- posterior(fitted)$topics %>% as.matrix
-  vocab <- colnames(phi)
-  doc_length <- vector()
-  for (i in 1:length(corpus)) 
-    if (!(i %in% zero_docs)){
-      temp <- paste(corpus[[i]]$content, collapse = ' ')
-      doc_length <- c(doc_length, stri_count(temp, regex = '\\S+'))
-    }
-  temp_frequency <- as.matrix(doc_term)
-  freq_matrix <- data.frame(ST = colnames(temp_frequency),
-                            Freq = colSums(temp_frequency))
-  rm(temp_frequency)
-  json_lda <- LDAvis::createJSON(phi = phi, theta = theta,
-                                 vocab = vocab,
-                                 doc.length = doc_length,
-                                 term.frequency = freq_matrix$Freq)
-  
-  
-  return(json_lda)
-}
-
-json10 <- topicmodels_json_ldavis(model10, docs_corpus, reduced.dtm)
-serVis(json10)
-
-dtm_topics <- topicmodels::topics(model10, 1)
+dtm_topics <- topicmodels::topics(model, 1)
 doctopics.df <- as.data.frame(dtm_topics)
-docs1<-docs1[rowTotals> 0, ]
-doctopics.df <- dplyr::transmute(doctopics.df, X = as.integer(rownames(doctopics.df)), Topic = dtm_topics)
-#docs1<-docs1[rowTotals> 0, ]
-#doctopics.df$id=docs1$id
-docs1 <- dplyr::inner_join(docs1, doctopics.df, by = "X")
+doctopics.df <- dplyr::transmute(doctopics.df, sha256 = as.character(rownames(doctopics.df)), Topic = dtm_topics)
+docs1 <- rbind(docs, docs_fr)
+docs1 <- dplyr::inner_join(docs1, doctopics.df, by = "sha256")
 
-#datasets
+#Visualization - topics distribution
+counts <- table(doctopics.df$Topic)
+counts <- counts *100 / sum(counts)
+head(sort(counts,decreasing = TRUE))
+barplot(counts, main=paste(modelsize, " topics"), 
+        col="darkblue")
 
-#documents - X, sha256,topic
-doc_topics <-docs1[,c("X", "sha256","Topic")]
+#Create datasets
+doc_topics <-docs1[,c("sha256","Topic")]
 write.csv(doc_topics, file = "doc_topics.csv")
 
-#topics - top 5 words
 topics=as.data.frame(t(dtm_terms[1:5,]))
 rownames(topics) <- NULL
 topics$label=topicLabel$Label
 write.csv(topics,file = "topics.csv")
-
-
